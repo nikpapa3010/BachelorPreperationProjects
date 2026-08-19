47bc22f29ab775488eafe691618651c478694776
Nikolaos Papadopoulos <nikpapa3010@gmail.com>
2026-08-19 18:59:21 +0200

    git init

---
+++ Source/PreperationProjects/PreperationProjects.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PreperationProjects.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PreperationProjects, "PreperationProjects" );
